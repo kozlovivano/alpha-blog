f4f29ff9c8dc282b037261c1ba1f512eece4ebfd
toshya <meunhoz@gmail.com>
2021-03-01 13:25:07 +1000

    Index page added, title unique constraint added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,17 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
   def create
-    #render plain: params[:article].inspect
+    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    if @article.save
-      flash[:notice] = "Article was created successfully"
+    if @article.save == 'true'
+      flash[:notice] = 'Article was created successfully'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -19,6 +23,7 @@ class ArticlesController < ApplicationController
   end
 
   private
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_154024) do
+ActiveRecord::Schema.define(version: 2021_03_01_030916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_154024) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["title"], name: "index_articles_on_title", unique: true
   end
 
 end
