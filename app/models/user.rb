255469c98680166e4357ffebb31f84f13829f2c1
toshya <meunhoz@gmail.com>
2021-03-02 06:56:41 +1000

    Email and user validation added to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: true
-  validates :email, presence: true
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX, message: 'Invalid email format' }
 end
