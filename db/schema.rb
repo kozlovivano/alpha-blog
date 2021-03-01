44ba8fb2f3caf5b04720e444d1f1249c484ab3f1
toshya <meunhoz@gmail.com>
2021-03-02 09:21:23 +1000

    Added secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_211637) do
+ActiveRecord::Schema.define(version: 2021_03_01_231723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_211637) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
