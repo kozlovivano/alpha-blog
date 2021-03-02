a8089e5db1c8b65e22d46336cfd870ef0008aefe
toshya <meunhoz@gmail.com>
2021-03-02 14:45:40 +1000

    Session controller added, user signin siginup signout logic added

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 
   resources :articles
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'signout', to: 'sessions#destroy'
+
   resources :users, except: [:new]
 end
