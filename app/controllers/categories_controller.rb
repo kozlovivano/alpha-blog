7fbc6c4bff7218b4bdf03144e28b5c1173b02a7d
toshya <meunhoz@gmail.com>
2021-03-03 17:08:52 +1000

    Integration test(category creating procedure user behavior test)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,27 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.all
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = 'Category created successfully'
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
