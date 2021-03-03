4441e95b5d0d55e763ef3b8bcd795960c572cd78
toshya <meunhoz@gmail.com>
2021-03-03 15:42:56 +1000

    Category test model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_060541) do
+ActiveRecord::Schema.define(version: 2021_03_02_093630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2021_03_02_060541) do
     t.datetime "updated_at"
     t.integer "user_id"
     t.index ["title"], name: "index_articles_on_title", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
