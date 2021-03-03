550b383aaa1bcf64e84721728697a32d9869595d
toshya <meunhoz@gmail.com>
2021-03-04 01:25:50 +1000

    Integration test(invalid category insert)

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -11,4 +11,14 @@ class CreateCategroiesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/index'
     assert_match 'sports', response.body
   end
+
+  test 'invalid category submission results in failure' do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: '' } }
+    end
+    assert_template 'categories/new'
+    assert_select 'div.alert-body'
+  end
 end
