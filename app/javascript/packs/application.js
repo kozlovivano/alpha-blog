dede3500714f06eb0e0b0217e79e0489621802d5
toshya <meunhoz@gmail.com>
2021-03-04 16:22:27 +1000

    Webpack issue fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,10 +13,4 @@ Turbolinks.start()
 ActiveStorage.start()
 
 
-require("bootstrap")
-document.addEventListener("turbolinks:load", function() {
-  $(function() {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
-  })
-})
+import "bootstrap"
